1bd4d4fee5eaecaacbb2b6fbf890d6e92c20c67a
Maksim <Maks.martinov98@yandex.ru>
2023-04-06 19:17:52 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 11500; // Стоимость билета
+        int bonus = 200; // За сколько рублей от билета будет начислятся одна миля
+
+        System.out.println("Ваш бонус составит:");
+        System.out.println((ticket / bonus) + " Миль");
+
+
+    }
+}
